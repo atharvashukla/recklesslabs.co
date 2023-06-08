84c13b7cc726d113a62904e6924fac391bcfef4b
Atharva Shukla <atharvashukla282@gmail.com>
2023-06-08 14:09:31 +0400

    Chronark -> Reckless

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,73 +5,72 @@ import { Metadata } from "next";
 import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
-	title: {
-		default: "chronark.com",
-		template: "%s | chronark.com",
-	},
-	description: "Software engineer at upstash.com and founder of planetfall.io",
-	openGraph: {
-		title: "chronark.com",
-		description:
-			"Software engineer at upstash.com and founder of planetfall.io",
-		url: "https://chronark.com",
-		siteName: "chronark.com",
-		images: [
-			{
-				url: "https://chronark.com/og.png",
-				width: 1920,
-				height: 1080,
-			},
-		],
-		locale: "en-US",
-		type: "website",
-	},
-	robots: {
-		index: true,
-		follow: true,
-		googleBot: {
-			index: true,
-			follow: true,
-			"max-video-preview": -1,
-			"max-image-preview": "large",
-			"max-snippet": -1,
-		},
-	},
-	twitter: {
-		title: "Chronark",
-		card: "summary_large_image",
-	},
-	icons: {
-		shortcut: "/favicon.png",
-	},
+  title: {
+    default: "recklesslabs.co",
+    template: "%s | recklesslabs.co",
+  },
+  description: "Reckless Labs founded by Atharva Shukla and Dhruv Aggarwal",
+  openGraph: {
+    title: "recklesslabs.co",
+    description: "Reckless Labs founded by Atharva Shukla and Dhruv Aggarwal",
+    url: "https://recklesslabs.co",
+    siteName: "recklesslabs.co",
+    images: [
+      {
+        url: "https://recklesslabs.co/og.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Reckless Labs",
+    card: "summary_large_image",
+  },
+  icons: {
+    shortcut: "/favicon.png",
+  },
 };
 const inter = Inter({
-	subsets: ["latin"],
-	variable: "--font-inter",
+  subsets: ["latin"],
+  variable: "--font-inter",
 });
 
 const calSans = LocalFont({
-	src: "../public/fonts/CalSans-SemiBold.ttf",
-	variable: "--font-calsans",
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
 });
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
-			<head>
-				<Analytics />
-			</head>
-			<body
-				className={`bg-black ${
-					process.env.NODE_ENV === "development" ? "debug-screens" : undefined
-				}`}
-			>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <head>
+        <Analytics />
+      </head>
+      <body
+        className={`bg-black ${
+          process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+        }`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
